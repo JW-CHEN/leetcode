d9a900a198f9260ee1416a97be89ca4e35a3cae5
jwchen <jwchen@jwchen-ThinkPad-X230.(none)>
2015-02-07 11:25:59 -0800

    add TreeNode structure and buildTree function in comm.h

--- common/comm.h
+++ common/comm.h
@@ -2,5 +2,40 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <stack>
+#include <queue>
 
 using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* buildTree(char testcase[], int len) {
+    TreeNode* root;
+    queue<TreeNode*> que;
+    root = new TreeNode((int)testcase[0] - 48);
+    que.push(root);
+    int ind = 1;
+    TreeNode* tmp;
+    while (!que.empty()) {
+        tmp = que.front();
+        que.pop();
+        if (ind >= len) 
+            return root;
+        if (testcase[ind] != '#') {
+            tmp->left = new TreeNode((int)testcase[ind] - 48);
+            que.push(tmp->left);
+        }
+        ind++;
+        if (testcase[ind] != '#') {
+            tmp->right = new TreeNode((int)testcase[ind] - 48);
+            que.push(tmp->right);
+        }
+        ind++;
+    }
+    return root;
+}
